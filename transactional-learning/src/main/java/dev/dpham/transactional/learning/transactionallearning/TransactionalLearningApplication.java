06311d1f94f59de4a1eb44678987263f0539a10a
David <dkt.pham@pm.me>
2024-04-04 20:07:35 +0200

    initial commit

---
+++ transactional-learning/src/main/java/dev/dpham/transactional/learning/transactionallearning/TransactionalLearningApplication.java
@@ -0,0 +1,13 @@
+package dev.dpham.transactional.learning.transactionallearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionalLearningApplication.class, args);
+	}
+
+}
---
+++ transactional-learning/src/test/java/dev/dpham/transactional/learning/transactionallearning/TransactionalLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dpham.transactional.learning.transactionallearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionalLearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
